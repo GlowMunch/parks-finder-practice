3244f357e9b1273cd0f09dbe16a9064a5ed44536
Evan Wheeler <evan.wheeler87@gmail.com>
2023-07-21 19:08:10 -0600

    Finish feature test and implement controller with api call to nps.

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -1,4 +1,17 @@
 class ParksController < ApplicationController
   def index
+    conn = Faraday.new(
+      url: 'https://developer.nps.gov',
+      headers: {
+        'Content-Type' => 'application/json',
+        'X-Api-Key' => Rails.application.credentials.nps_api_key
+      }
+    )
+
+    response = conn.get('/api/v1/parks') do |req|
+      req.params['stateCode'] = 'TN'
+    end
+
+    @parks_data = JSON.parse(response.body)
   end
 end
--- spec/features/parks_list_spec.rb
+++ spec/features/parks_list_spec.rb
@@ -2,6 +2,13 @@ require 'rails_helper'
 
 feature 'Parks list' do
   scenario 'can be filtered by state' do
+    parks_json = file_fixture('parks.json').read
+    parks_response = JSON.parse(parks_json)
+
+    stub_request(:get, "https://developer.nps.gov/api/v1/parks?stateCode=TN")
+    .with(headers: {'X-Api-Key' => Rails.application.credentials.nps_api_key})
+    .to_return(body: parks_json)
+
     # As a user,
     visit root_path
     # When I select "Tennessee" from the form,
@@ -10,16 +17,16 @@ feature 'Parks list' do
     click_button 'Find Parks'
 
     # I see the total of parks found,
-    expect(page).to have_content "Total Parks: #{10}"
+    expect(page).to have_content "Total Parks: #{parks_response['total']}"
     # And for each park I see:
     first_park = page.first(:css, '.park')
     # - full name of the park
-    expect(first_park).to have_content ''
+    expect(first_park).to have_content parks_response['data'][0]['fullName']
     # - description
-    expect(first_park).to have_content ''
+    expect(first_park).to have_content parks_response['data'][0]['description']
     # - direction info
-    expect(first_park).to have_content ''
+    expect(first_park).to have_content parks_response['data'][0]['directionsInfo']
     # - standard hours for operation
-    expect(first_park).to have_content ''
+    expect(first_park).to have_content parks_response['data'][0]['operatingHours'][0]['standardHours']
   end
 end
